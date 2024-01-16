8f86755b786e4a70d8f6a1e19a6b1f81189eae66
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2024-01-16 11:26:04 -0600

    Initial Commit

---
+++ todo-frontend/todo_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_frontendApp.swift
+//  todo-frontend
+//
+//  Created by Jorge on 16/01/24.
+//
+
+import SwiftUI
+
+@main
+struct todo_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
